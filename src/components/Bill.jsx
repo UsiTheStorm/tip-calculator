44b6c0916843f17e1046b55a6a38c0571df12c18
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-29 13:21:51 +0100

    Make Bill input controlled and add props
    Refactored the Bill component to accept 'bill' and 'onSetBill' as props, making the input a controlled component. Added a max value to the input and updated the placeholder format.

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 
-function Bill() {
+function Bill({ onSetBill, bill }) {
   return (
     <div>
       <label htmlFor="bill" className="label">
         How much was your bill?
       </label>
-      <input type="number" id="bill" min={0} className="input" placeholder="10$" />
+      <input
+        type="number"
+        id="bill"
+        min={0}
+        max={10000}
+        className="input"
+        placeholder="$10"
+        value={bill}
+        onChange={(e) => onSetBill(e.target.value)}
+      />
     </div>
   );
 }
